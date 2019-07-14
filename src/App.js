9f8fb6f714821b61ca18f591d1a53b0d730af3eb
mounikr23 <mounik.reddy23@gmail.com>
2019-07-14 16:30:33 +0530

    initial render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Modal from 'react-modal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal
+        className="Modal"
+        overlayClassName="overlay"
+        isOpen={true}>
+        <div className="ModalHeadingWrapper">
+          <h2 className="Heading">Setting up categories and attributes for project</h2>
+          <p className="Description">This includes setting up categories, file attributes and label attributes with custom keyboard shortcuts for each label.</p>
+        </div>
+      </Modal>
     </div>
   );
 }
